6eea58733a21e6c227629f30723b8c975fd77ada
Daniel Fröschl <danielfroeschl@gmx.at>
2021-10-14 22:53:31 +0200

    Add ITableStorageManagement

---
+++ src/EarthLat.Backend.Core.Abstraction/ITableStorageManagement.cs
@@ -0,0 +1,11 @@
+﻿namespace EarthLat.Backend.Core.Abstraction
+{
+    public interface ITableStorageManagement
+    {
+        void CreateTable(string tableName);
+        void EnsureTable(string tableName);
+        IEnumerable<string> GetTables();
+        string GetTable(string tableName);
+        void DeleteTable(string tableName);
+    }
+}
